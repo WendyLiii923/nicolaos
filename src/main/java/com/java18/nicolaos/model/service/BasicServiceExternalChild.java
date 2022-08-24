f268d7bfc05f3eac403598322d41cf13ecb03847
Wendy <romiling923@gmail.com>
2022-08-24 14:47:11 +0800

    added BasicServiceExternalChild

---
+++ src/main/java/com/java18/nicolaos/model/service/BasicServiceExternalChild.java
@@ -0,0 +1,17 @@
+package com.java18.nicolaos.model.service;
+
+public class BasicServiceExternalChild {
+
+	public static void main(String[] args) {
+		BasicServiceChild bsc = new BasicServiceChild();
+//		int ans1 = bsc.funcP1(6, 5);
+//		System.out.println(ans1);
+		
+		int ans2 = bsc.funcP2(6, 5);
+		System.out.println(ans2);
+		
+		int ans3 = bsc.funcP3(6, 5);
+		System.out.println(ans3);
+	}
+
+}
